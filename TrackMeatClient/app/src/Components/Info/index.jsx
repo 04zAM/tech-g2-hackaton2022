425aad73a2a17d298488588e348db407a8437084
Esteban <pvasquezc@farmaenlace.com>
2022-11-12 23:07:52 -0500

    Avance y creacion de la pagina de lectura de trazabilidad

---
+++ TrackMeatClient/app/src/Components/Info/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 col-sm-12">
+                    <h3 className="text-center">Código de Lote: #CF2332S</h3>
+                </div>
+            </div>
+        </div>
+    );
+}
