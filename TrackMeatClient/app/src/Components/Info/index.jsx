9a5d7daab3bb418b68467f28dc9e7bc006b4a89c
Esteban <pvasquezc@farmaenlace.com>
2022-11-13 05:00:55 -0500

    Avance v3

--- TrackMeatClient/app/src/Components/Info/index.jsx
+++ TrackMeatClient/app/src/Components/Info/index.jsx
@@ -6,6 +6,14 @@ export default function Home() {
             <div className="row">
                 <div className="col-md-6 offset-md-3 col-sm-12">
                     <h3 className="text-center">Código de Lote: #CF2332S</h3>
+                    <h3 className="text-center">Código Granja: #GA243P Nombre: Granja Catalina </h3>
+                    <h3 className="text-center">Código Ganado(Oreja): #GF2332S Nombre: Juanita1</h3>
+                    <h3 className="text-center">Código de Alimentación: #CA2332S Tipo: Balanceado Super</h3>
+                    <h3 className="text-center">Código de Cuidado: #CC2332H Vacunas: Derri A Plus, Brucel RB51 Plus, Melirev-R, Nobivac Rabia</h3>
+                    <h3 className="text-center">Código de Corte: #CO2332S Tipo: Costillar Ambiente: Estéril</h3>
+                    <img className="text-center" alt="cortes" src="http://3.bp.blogspot.com/-8iHRuBqmciE/Tz8p_9kjwpI/AAAAAAAAAKw/gNbWZrLHBfM/s1600/ternera.jpg"/>
+                    <h3 className="text-center">Código de Auditoria: #A2332S Empresa Auditora: AuditiCarnicos</h3>
+                    <h3 className="text-center">Código de Certificado: #CERT2332 Norma: ISO-90001 Empresa Certificadora: Certifast</h3>
                 </div>
             </div>
         </div>
---
+++ TrackMeatClient/app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBcOgo134OUGSZbvluHXKfn-MQJsyaWUQc",
+    authDomain: "trackmeatclient.firebaseapp.com",
+    projectId: "trackmeatclient",
+    storageBucket: "trackmeatclient.appspot.com",
+    messagingSenderId: "1089451841153",
+    appId: "1:1089451841153:web:8472c5908f850247751f80"
+  };
